d1feb1a99c975c12f650fa973873d31c005ded87
varshitha <varshitha.k.jayappa@gmail.com>
2025-11-06 11:45:44 +0100

    adding jenkinsfile

--- client/src/axios.js
+++ client/src/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: '/api', // Proxy from frontend NGINX to backend K8s service
+    //baseURL: 'http://localhost:/api'
 });
 
 instance.interceptors.request.use((config) => {
